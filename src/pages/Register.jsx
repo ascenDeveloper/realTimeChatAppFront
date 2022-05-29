4e49a612d3db466796643533688ca009058f51fa
Ascended300 <eduardofelix014@gmail.com>
2022-05-28 21:19:10 -0300

    Forma del registe terminada, sin lógica

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import style from 'styled-components'
+import { Link } from 'react-router-dom'
 
 function Register() {
 
 
+    const handleChange = (event) => {
+        event.preventDefault()
+        alert('form')
+    }
     const handleSubmit = (event) => {
         event.preventDefault()
         alert('form')
@@ -21,8 +26,30 @@ function Register() {
                         type="text"
                         placeholder='Username'
                         name='username'
-
+                        onChange={e => handleChange(e)}
                     />
+                    <input
+                        type="email"
+                        placeholder='Email'
+                        name='email'
+                        onChange={e => handleChange(e)}
+                    />
+                    <input
+                        type="password"
+                        placeholder='Password'
+                        name='password'
+                        onChange={e => handleChange(e)}
+                    />
+                    <input
+                        type="password"
+                        placeholder='Confirm Password'
+                        name='confirmPassword'
+                        onChange={e => handleChange(e)}
+                    />
+                    <button type='submit'>Create User</button>
+                    <span>
+                        already have an account? <Link to='/login'>Login</Link>
+                    </span>
                 </form>
 
             </FormContainer>
