42c4e9b074d52bb9d23a847273657af5c1948a78
Ascended300 <eduardofelix014@gmail.com>
2022-05-30 00:44:34 -0300

    Register con compreobaciones, añadido axios

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,18 +1,52 @@
-import React from 'react'
-import style from 'styled-components'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import Logo from '../assets/logo.svg'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import axios from 'axios'
 
 function Register() {
-
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    })
+    const toastOptions = {
+        position: 'bottom-right',
+        autoClose: 3000,
+        pauseOnHover: false,
+        draggable: true,
+        theme: 'dark',
+    }
 
     const handleChange = (event) => {
-        event.preventDefault()
-        alert('form')
+        setValues({ ...values, [event.target.name]: event.target.value })
     }
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault()
-        alert('form')
+        if (handleValidation()) {
+            const { password, confirmPassword, username, email } = values
+            const { data } = await axios.post()
+        }
+    }
+    const handleValidation = () => {
+        const { password, confirmPassword, username, email } = values
+        if (password !== confirmPassword) {
+            toast.error('password and confirm password should be same.', toastOptions);
+            return false
+        } else if (username.length < 3) {
+            toast.error('Username should be greater than a 3 characters', toastOptions)
+            return false
+        } else if (password.length < 8) {
+            toast.error('Password should be equal or greater than a 8 characters', toastOptions)
+            return false
+        } else if (email === '') {
+            toast.error('Email is required', toastOptions)
+            return false
+        }
+        return true
     }
 
     return (
@@ -54,13 +88,82 @@ function Register() {
                 </form>
 
             </FormContainer>
+            <ToastContainer />
         </>
     )
 }
 
-const FormContainer = style.div`
+const FormContainer = styled.div`
     height: 100vh;
     width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+    align-items: center;
+    background-color: #131324;
+    .brand {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        justify-content: center;
+        img{
+            height: 5rem;
+        }
+        h1 {
+            color: white;
+            text-transform: uppercase;
+        }
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        background-color: #00000076;
+        border-radius: 2rem;
+        padding: 3rem 5rem;
+        input {
+            background-color: transparent;
+            padding: 1rem;
+            border: 0.1rem solid #4e0eff;
+            border-radius: 0.4rem;
+            color: white;
+            width: 100%;
+            font-size: 1rem;
+            &:-webkit-autofill{  /*Data  necesaria para quitar background del autocompletado de los inputs, RECORDAR O TENER EN CUENTA */ 
+                transition: background-color 5000s;
+                -webkit-text-fill-color: #fff;
+            }
+            &:focus{
+                border:0.1rem solid #997af0;
+                outline: none;
+            }
+        }
+        button {
+            background-color: #997af0;
+            color: white;
+            padding: 1rem 2rem;
+            border: none;
+            font-weight: bold;
+            cursor: pointer;
+            border-radius: 0.4rem;
+            font-size: 1rem;
+            text-transform: uppercase;
+            transition: 0.3s ease-in-out;
+            &:hover {
+                background-color: #4e0eff;
+            }
+        }
+        span {
+            color: white;
+            text-transform: uppercase;
+            a {
+                color: #4e0eff;
+                text-decoration: none;
+                font-weight: bold
+            }
+        }
+    }
 `
 
 export default Register
